051390d0049ad09d31fb6850bac78d1e492a5789
ralph <github@ralph-schuster.eu>
2020-07-12 10:18:41 +0200

    Beautifying

--- bin/run.bat
+++ bin/run.bat
@@ -5,9 +5,9 @@
 @FOR %%a IN ("%BASENAME:~0,-1%") DO @SET ROOTDIR=%%~dpa
 
 @SET CLASSPATH=%ROOTDIR%resources
-@for %%i in (%ROOTDIR%*.jar) DO @SET CLASSPATH=%CLASSPATH%;%%i
+@FOR %%i IN (%ROOTDIR%*.jar) DO @SET CLASSPATH=%CLASSPATH%;%%i
 
-@for %%i in (%ROOTDIR%lib\*.jar) DO @CALL SET CLASSPATH=!CLASSPATH!;%%i
+@FOR %%i IN (%ROOTDIR%lib\*.jar) DO @CALL SET CLASSPATH=!CLASSPATH!;%%i
 
 @java -cp %CLASSPATH% templating.Templating %*
-pause
+@PAUSE
