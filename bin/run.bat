00e637282e635a62c1d25565e6a9c4d865b7c092
technicalguru <github@ralph-schuster.eu>
2021-01-12 11:10:44 +0100

    #21 - Fix handling of spaces in path names

--- bin/run.bat
+++ bin/run.bat
@@ -5,9 +5,9 @@
 @FOR %%a IN ("%BASENAME:~0,-1%") DO @SET ROOTDIR=%%~dpa
 
 @SET CLASSPATH=%ROOTDIR%resources
-@FOR %%i IN (%ROOTDIR%*.jar) DO @SET CLASSPATH=%CLASSPATH%;%%i
+@FOR %%i IN ("%ROOTDIR%*.jar") DO @SET CLASSPATH=%CLASSPATH%;%%i
 
-@FOR %%i IN (%ROOTDIR%lib\*.jar) DO @CALL SET CLASSPATH=!CLASSPATH!;%%i
+@FOR %%i IN ("%ROOTDIR%lib\*.jar") DO @CALL SET CLASSPATH=!CLASSPATH!;%%i
 
-@java -cp %CLASSPATH% templating.TemplateMachine %*
+@java -cp "%CLASSPATH%" templating.TemplateMachine %*
 @PAUSE
